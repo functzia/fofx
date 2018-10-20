f4864a7c8a172ba05a84e6d16f8ce404d75a3322
Alon Niv <oakfang@gmail.com>
2018-10-21 02:05:00 +0300

    v2

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
 const EventEmitter = require('events');
-const _ = require('lodash');
+const rimraf = require('rimraf');
+const pacote = require('pacote');
+const npm = require('npm/lib/npm.js');
+const npmInstall = require('npm/lib/install.js');
+const prequire = require('./plugins');
 
 const promisify = fn => (...args) =>
   new Promise((resolve, reject) =>
@@ -15,6 +19,8 @@ async function getJsonFromFile(filePath) {
   const content = await promisify(fs.readFile)(filePath, { encoding: 'utf8' });
   return JSON.parse(content);
 }
+
+const rimrafAP = promisify(rimraf);
 
 function getFunctionsQueue() {
   const fns = {};
@@ -45,12 +51,44 @@ function getFunctionsQueue() {
   };
 }
 
+async function installPlugins(pluginList) {
+  await promisify(npm.load)({
+    loglevel: 'silent',
+    progress: false,
+    silent: true
+  });
+  return new Promise((resolve, reject) => {
+    npmInstall(
+      './plugins',
+      pluginList,
+      (err, result) => (err ? reject(err) : resolve(result))
+    );
+  });
+}
+
 async function main() {
   const fq = getFunctionsQueue();
-  const pluginsByType = _.keyBy(
-    (await getJsonFromFile('./demo/plugins.json')).map(require),
-    'type'
+  await rimrafAP('./cache');
+  await rimrafAP('./plugins/node_modules');
+  await rimrafAP('./plugins/package-lock.json');
+  const pluginsSpecs = await getJsonFromFile('./demo/plugins.json');
+  const exPluginsSpecs = pluginsSpecs.map(
+    spec => (typeof spec === 'string' ? { name: spec } : spec)
   );
+  const specNames = exPluginsSpecs.map(spec => spec.name);
+  const [manifests] = await Promise.all([
+    await Promise.all(
+      specNames.map(spec => pacote.manifest(spec, { cache: './cache' }))
+    ),
+    await installPlugins(specNames)
+  ]);
+  const pluginsByType = manifests.reduce((plg, { name }, idx) => {
+    const pluginFactory = prequire(name);
+    const config = exPluginsSpecs[idx].params || {};
+    const plugin = pluginFactory(config);
+    plg[plugin.type] = plugin;
+    return plg;
+  }, {});
   const fnsPaths = await getJsonFromFile('./demo/fns.json');
   const fns = fnsPaths.reduce(
     (map, path) =>
@@ -68,6 +106,7 @@ async function main() {
     // const alertOutput = (...args) => resolver.emit(fn + '/output', ...args);
     fq.recordFunction(fn, app);
   });
+  console.log('Ready To go');
 }
 
 main().catch(console.error);
---
+++ plugins/index.js
@@ -0,0 +1,3 @@
+module.exports = function requirePlugin(pluginName) {
+  return require(pluginName);
+};
