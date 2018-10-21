f494a2771e6d0c6dd1781ddf04366265d547cf34
Alon Niv <oakfang@gmail.com>
2018-10-21 18:41:42 +0300

    Utilise logs

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const getNanosQueue = require('./lib/queue');
 const { getPluginsByType } = require('./lib/plugins');
 const { loadNanos } = require('./lib/nanos');
+const Logger = require('./lib/logger');
+const logText = require('./lib/logger/text');
 
-async function main() {
-  const nq = getNanosQueue();
-  const pluginsByType = await getPluginsByType('./demo/plugins.json');
-  await loadNanos('./demo/nanos.json', pluginsByType, nq);
-  console.log('Ready To go');
+async function setupFofx(logLevel, pluginsFile, nanosFile) {
+  const log = new Logger(Logger.levels[logLevel.toUpperCase()]);
+  logText(log);
+  const fofxLog = log.scoped('fofx');
+  try {
+    const nq = getNanosQueue(log);
+    const pluginsByType = await getPluginsByType(pluginsFile, log);
+    await loadNanos(nanosFile, pluginsByType, nq);
+    fofxLog.info('Ready to go!');
+  } catch (error) {
+    fofxLog.fatal('Fatal platform error');
+    fofxLog.fatal(error);
+  }
 }
 
-main().catch(console.error);
+setupFofx(...process.argv.slice(2)).catch(console.error);
