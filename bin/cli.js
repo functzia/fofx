04d5bf2811ea15225fe2199a3ecf4b2ac47604c8
Alon Niv <oakfang@gmail.com>
2018-10-23 00:46:23 +0300

    Minor cli fixes

--- bin/cli.js
+++ bin/cli.js
@@ -1,24 +1,33 @@
 #! /usr/bin/env node
 const path = require('path');
 const { argv } = require('yargs')
-  .usage(
-    'Usage: $0 --plugins [plugins file] --nanos [nanos file] [--level=INFO]'
-  )
+  .usage('Usage: fofx [...options] [--watch]')
+  .boolean('watch')
+
   .default('level', 'INFO')
   .default('modules', path.join(process.cwd(), 'modules'))
   .default('plugins', path.join(process.cwd(), 'plugins.json'))
   .default('nanos', path.join(process.cwd(), 'nanos.json'))
-  .describe('plugins', 'Path to plugins JSON file')
-  .describe('nanos', 'Path to nanos JSON file')
-  .describe('modules', 'Directory in which to save dependencies for runtime')
+
+  .describe('watch', 'Watch plugins and nanos files for changes, and reload')
   .describe(
     'level',
     'Minimal log level to print (DOC|DEBUG|INFO|WARN|ERROR|FATAL)'
-  );
+  )
+  .describe('plugins', 'Path to plugins JSON file')
+  .describe('nanos', 'Path to nanos JSON file')
+  .describe('modules', 'Directory in which to save dependencies for runtime');
+
 const startGB = require('./green-blue');
 const setupFofx = require('..');
 
 const main = () => setupFofx(argv).catch(console.error);
-startGB([argv.plugins, argv.nanos], main, 10000, () =>
-  console.log('fofx detected changes in your configuration files, reloading...')
-);
+if (!argv.watch) {
+  main();
+} else {
+  startGB([argv.plugins, argv.nanos], main, 10000, () =>
+    console.log(
+      'fofx detected changes in your configuration files, reloading...'
+    )
+  );
+}
