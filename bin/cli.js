9a12fd01e112e058ae4f12dcd2f0d0a6edd13a85
oakfang <oakfang@gmail.com>
2019-04-18 14:31:35 +0300

    Initial CLI release

--- bin/cli.js
+++ bin/cli.js
@@ -1,33 +1,53 @@
 #! /usr/bin/env node
 const path = require('path');
-const { argv } = require('yargs')
-  .usage('Usage: fofx [...options] [--watch]')
-  .boolean('watch')
-  .boolean('dry')
-  .conflicts('watch', 'port')
-
-  .default('level', 'INFO')
-  .default('modules', path.join(process.cwd(), 'modules'))
-  .default('manifest', path.join(process.cwd(), 'package.json'))
-
-  .describe('watch', 'Watch plugins and nanos files for changes, and reload')
-  .describe('port', 'Run fofx as a master, using a local websocket server')
-  .describe(
-    'level',
-    'Minimal log level to print (DOC|DEBUG|INFO|WARN|ERROR|FATAL)'
-  )
-  .describe('manifest', 'Path to package.json file')
-  .describe('modules', 'Directory in which to save dependencies for runtime')
-  .describe('dry', "Don't actually install anything");
-
+const yargs = require('yargs');
+const startMaster = require('@fofx/core');
+const { createLogger, textMode } = require('@fofx/logger');
 const startGB = require('./green-blue');
-const setupFofx = require('..');
 
-const main = () => setupFofx(argv).catch(console.error);
-if (!argv.watch) {
+function parseArguments() {
+  const {
+    argv: { watch, dry, port, level, modules, manifest },
+  } = yargs
+    .usage('Usage: fofx [...options] [--watch]')
+    .boolean('watch')
+    .boolean('dry')
+    .conflicts('watch', 'port')
+
+    .default('level', 'INFO')
+    .default('modules', path.join(process.cwd(), 'modules'))
+    .default('manifest', path.join(process.cwd(), 'package.json'))
+
+    .describe('watch', 'Watch plugins and nanos files for changes, and reload')
+    .describe('port', 'Run fofx as a master, using a local websocket server')
+    .describe(
+      'level',
+      'Minimal log level to print (DOC|DEBUG|INFO|WARN|ERROR|FATAL)'
+    )
+    .describe('manifest', 'Path to package.json file')
+    .describe('modules', 'Directory in which to save dependencies for runtime')
+    .describe('dry', "Don't actually install anything");
+  return { watch, dry, port, level, modules, manifest };
+}
+
+const { watch, dry, port, level, modules, manifest } = parseArguments();
+
+async function main() {
+  try {
+    const { fofx: fofxConfig } = require(manifest);
+    const install = !dry;
+    const log = createLogger();
+    log.setMode(textMode, createLogger.logLevels[level]);
+    await startMaster({ log, fofxConfig, modulesDir: modules, port, install });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+if (!watch) {
   main();
 } else {
-  startGB([argv.manifest], main, 10000, () =>
+  startGB([manifest], main, 10000, () =>
     console.log(
       'fofx detected changes in your configuration files, reloading...'
     )
--- bin/worker-cli.js
+++ bin/worker-cli.js
@@ -1,25 +1,36 @@
 #! /usr/bin/env node
 const path = require('path');
-const { argv } = require('yargs')
-  .usage(
-    'Usage: fofx-worker [...options] <--broker="wss?://host:port">'
-  )
-  .required('broker')
+const yargs = require('yargs');
+const startWorker = require('@fofx/core/worker');
+const { createLogger, textMode } = require('@fofx/logger');
 
-  .boolean('dry')
+function parseArguments() {
+  const {
+    argv: { broker, dry, level, manifest, modules },
+  } = yargs
+    .usage('Usage: fofx-worker [...options] <--broker="wss?://host:port">')
+    .required('broker')
 
-  .default('level', 'INFO')
-  .default('manifest', path.join(process.cwd(), 'package.json'))
-  .default('modules', path.join(process.cwd(), 'modules'))
+    .boolean('dry')
 
-  .describe('broker', 'Broker connections string (e.g. ws://localhost:9999)')
-  .describe(
-    'level',
-    'Minimal log level to print (DOC|DEBUG|INFO|WARN|ERROR|FATAL)'
-  )
-  .describe('manifest', 'Path to package.json file')
-  .describe('dry', "Don't install the nanos")
-  .describe('modules', 'Directory in which to save dependencies for runtime');
+    .default('level', 'INFO')
+    .default('manifest', path.join(process.cwd(), 'package.json'))
+    .default('modules', path.join(process.cwd(), 'modules'))
 
-const startWorker = require('../worker');
-startWorker(argv);
+    .describe('broker', 'Broker connections string (e.g. ws://localhost:9999)')
+    .describe(
+      'level',
+      'Minimal log level to print (DOC|DEBUG|INFO|WARN|ERROR|FATAL)'
+    )
+    .describe('manifest', 'Path to package.json file')
+    .describe('dry', "Don't install the nanos")
+    .describe('modules', 'Directory in which to save dependencies for runtime');
+  return { broker, dry, level, manifest, modules };
+}
+
+const { broker, dry, level, manifest, modules } = parseArguments();
+const { fofx: fofxConfig } = require(manifest);
+const install = !dry;
+const log = createLogger();
+log.setMode(textMode, createLogger.logLevels[level]);
+startWorker({ log, fofxConfig, modulesDir: modules, broker, install });
